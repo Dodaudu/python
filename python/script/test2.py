33d3a11da9ea572eb07eda20c3d8b53d26b8731b
David <davidodaudu10@gmail.com>
2022-11-05 19:37:56 +0000

    Add: Added learning for week 2

---
+++ python/script/test2.py
@@ -0,0 +1,16 @@
+name=input("what is youre name? ")
+print(f"hello {name}")
+age=input("how old are you? ")
+age_af5=int(age)+5
+print( f"{name} will be {age_af5} in 5 years" )
+print(""" this code is brought to you by david
+
+-my regards david
+""")
+
+
+red="David is amazing"
+print(red.upper())
+print(red.lower())
+print(red.find("d"))
+print(red.find("david"))
---
+++ python/script/testing.py
@@ -0,0 +1,4 @@
+date=input("what year were you born? ")
+date=int(date)
+age=2022-date
+print(("david is ") +str (age))
